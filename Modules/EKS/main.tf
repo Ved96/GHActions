a12089c37ad13d29963de169b2ab4777510bfdc3
Ved96 <vedant.kulkarni5896@gmail.com>
2023-10-10 16:26:55 +0530

    asfd

--- Modules/EKS/main.tf
+++ Modules/EKS/main.tf
@@ -1,5 +1,5 @@
 ######################### aws_eks_cluster ####################################
-###asdkfhhf###
+###asdkfhhf### assafdsdaf
 resource "aws_eks_cluster" "eks_cluster" {
   name                      = var.cluster_name
   role_arn                  = var.eks_cluster_role_arn
